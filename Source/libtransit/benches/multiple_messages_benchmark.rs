c16841b792f729a2abea0fcadd29c7c11d843747
Alex <40502404+Alex-Programs@users.noreply.github.com>
2023-07-28 12:01:01 +0100

    Rewrote libtransit

--- Source/libtransit/benches/multiple_messages_benchmark.rs
+++ Source/libtransit/benches/multiple_messages_benchmark.rs
@@ -1,6 +1,6 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
-use libtransit::{Message, CreateSocketMessage, StreamMessage, CloseSocketMessage, MessageType, MultipleMessages};
+use libtransit::{Message, UpStreamMessage, DownStreamMessage, MultipleMessagesUpstream, MultipleMessagesDownstream, CloseSocketMessage};
 
 fn gen_data(size: usize) -> Vec<u8> {
     // Generate random data
@@ -13,69 +13,80 @@ fn gen_data(size: usize) -> Vec<u8> {
     data
 }
 
-fn gen_messages(messagecount: u32, data: Vec<u8>) -> Vec<Message> {
-    let mut messages: Vec<Message> = Vec::new();
+fn gen_upstream_messages(messagecount: u32, data: Vec<u8>) -> Vec<UpStreamMessage> {
+    let mut messages: Vec<UpStreamMessage> = Vec::new();
 
     for i in 0..messagecount {
-        messages.push(Message::CreateSocketMessage(CreateSocketMessage {
-            message_type: MessageType::CreateSocket,
+        let message = UpStreamMessage {
             socket_id: i,
-            forwarding_address: 43,
-            forwarding_port: 80,
-        }));
+            message_sequence_number: i,
+            dest_ip: 0,
+            dest_port: 0,
+            payload: data.clone(),
+        };
 
-        messages.push(Message::StreamMessage(StreamMessage {
-            message_type: MessageType::SendStreamPacket,
-            socket_id: i,
-            send_buffer_size: 0,
-            receive_buffer_size: 0,
-            packet_data: data.clone(),
-        }));
-
-        messages.push(Message::CloseSocketMessage(CloseSocketMessage {
-            message_type: MessageType::CloseSocket,
-            socket_id: i,
-        }));
+        messages.push(message);
     }
 
     messages
 }
 
-fn stress_test_multiple_messages(messages: Vec<Message>) {
-    let multiple_messages = MultipleMessages::from_messages(messages.clone());
+fn gen_close_sockets(messagecount: u32) -> Vec<CloseSocketMessage> {
+    let mut messages: Vec<CloseSocketMessage> = Vec::new();
 
-    let buffer = multiple_messages.to_bytes();
+    for i in 0..messagecount {
+        let message = CloseSocketMessage {
+            socket_id: i,
+            message_sequence_number: i,
+        };
 
-    let multiple_messages = MultipleMessages::from_bytes(buffer);
+        messages.push(message);
+    }
 
-    let messages = multiple_messages.pull_messages();
+    messages
+}
 
-    assert_eq!(messages, messages);
+fn stress_test_multiple_messages_upstream(upstreams: Vec<UpStreamMessage>, close_sockets: Vec<CloseSocketMessage>) {
+    let multiple_messages_up = MultipleMessagesUpstream {
+        stream_messages: upstreams,
+        close_socket_messages: close_sockets,
+    };
+
+    let mut encoded = multiple_messages_up.encoded().unwrap();
+
+    let decoded = MultipleMessagesUpstream::decode_from_bytes(&mut encoded).unwrap();
+
+    assert_eq!(multiple_messages_up, decoded);
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let messages = gen_messages(50, gen_data(100));
-    c.bench_function("stress_test_multiple_messages_50mc_100b    (5kb)", |b| b.iter(|| stress_test_multiple_messages(messages.clone())));
+    let stream_messages = gen_upstream_messages(50, gen_data(100));
+    let close_socket_messages = gen_close_sockets(10);
+    c.bench_function("stress_test_multiple_messages_50mc_100b    (5kb)", |b| b.iter(|| stress_test_multiple_messages_upstream(stream_messages.clone(), close_socket_messages.clone())));
     
-    let messages = gen_messages(50, gen_data(1000));
-    c.bench_function("stress_test_multiple_messages_50mc_1kb     (50kb)", |b| b.iter(|| stress_test_multiple_messages(messages.clone())));
+    let stream_messages = gen_upstream_messages(50, gen_data(1_000));
+    let close_socket_messages = gen_close_sockets(10);
+    c.bench_function("stress_test_multiple_messages_50mc_1kb     (50kb)", |b| b.iter(|| stress_test_multiple_messages_upstream(stream_messages.clone(), close_socket_messages.clone())));
     
-    let messages = gen_messages(50, gen_data(10_000));
-    c.bench_function("stress_test_multiple_messages_50mc_10kb    (500kb)", |b| b.iter(|| stress_test_multiple_messages(messages.clone())));
-    
+    let stream_messages = gen_upstream_messages(50, gen_data(10_000));
+    let close_socket_messages = gen_close_sockets(10);
+    c.bench_function("stress_test_multiple_messages_50mc_10kb    (500kb)", |b| b.iter(|| stress_test_multiple_messages_upstream(stream_messages.clone(), close_socket_messages.clone())));
 
-    
-    let messages = gen_messages(50, gen_data(100_000));
-    c.bench_function("stress_test_multiple_messages_50mc_100kb   (5mb)", |b| b.iter(|| stress_test_multiple_messages(messages.clone())));
+    let stream_messages = gen_upstream_messages(50, gen_data(100_000));
+    let close_socket_messages = gen_close_sockets(10);
+    c.bench_function("stress_test_multiple_messages_50mc_100kb   (5mb)", |b| b.iter(|| stress_test_multiple_messages_upstream(stream_messages.clone(), close_socket_messages.clone())));
 
-    let messages = gen_messages(10, gen_data(1_000_000));
-    c.bench_function("stress_test_multiple_messages_10mc_1mb   (10mb)", |b| b.iter(|| stress_test_multiple_messages(messages.clone())));
+    let stream_messages = gen_upstream_messages(10, gen_data(1_000_000));
+    let close_socket_messages = gen_close_sockets(10);
+    c.bench_function("stress_test_multiple_messages_10mc_1mb     (10mb)", |b| b.iter(|| stress_test_multiple_messages_upstream(stream_messages.clone(), close_socket_messages.clone())));
     
-    let messages = gen_messages(10, gen_data(10_000_000));
-    c.bench_function("stress_test_multiple_messages_10mc_10mb  (100mb)", |b| b.iter(|| stress_test_multiple_messages(messages.clone())));
-    
-    let messages = gen_messages(10, gen_data(100_000_000));
-    c.bench_function("stress_test_multiple_messages_10mc_100mb (1000mb)", |b| b.iter(|| stress_test_multiple_messages(messages.clone())));
+    let stream_messages = gen_upstream_messages(10, gen_data(10_000_000));
+    let close_socket_messages = gen_close_sockets(10);
+    c.bench_function("stress_test_multiple_messages_50mc_10mb    (100mb)", |b| b.iter(|| stress_test_multiple_messages_upstream(stream_messages.clone(), close_socket_messages.clone())));
+
+    let stream_messages = gen_upstream_messages(10, gen_data(100_000_000));
+    let close_socket_messages = gen_close_sockets(10);
+    c.bench_function("stress_test_multiple_messages_50mc_100mb   (1gb)", |b| b.iter(|| stress_test_multiple_messages_upstream(stream_messages.clone(), close_socket_messages.clone())));
 }
 
 criterion_group!(benches, criterion_benchmark);
--- Source/libtransit/src/lib.rs
+++ Source/libtransit/src/lib.rs
@@ -1,361 +1,232 @@
-use binrw;
-use binrw::{BinReaderExt, BinWriterExt};
-use binrw::io::Cursor;
 use rand::Rng; // Not unused - ignore vscode
+use borsh::{BorshSerialize, BorshDeserialize};
 
 pub type Port = u16;
 pub type IPV4 = u32;
 pub type SocketID = u32;
 
-#[binrw::binrw]
-#[derive(Debug, PartialEq)]
-#[brw(big)]
-pub struct MultipleMessages {
-    #[bw(try_calc(u8::try_from(sizes.len())))]
-    pub sizes_length: u8,
-    #[br(count = sizes_length)]
-    pub sizes: Vec<u32>,
+static LAST_SOCKET_ID: std::sync::atomic::AtomicU32 = std::sync::atomic::AtomicU32::new(0);
 
-    #[bw(try_calc(u32::try_from(data.len())))]
-    pub data_length: u32,
-    #[br(count = data_length)]
-    pub data: Vec<u8>,
-}
+// TODO look at using slices for performance
 
-impl MultipleMessages {
-    pub fn from_messages(messages: Vec<Message>) -> Self {
-        if messages.len() > u8::MAX as usize {
-            panic!("Cannot encode more than 255 messages into a single buffer.");
-        }
-    
-        let mut sizes: Vec<u32> = Vec::with_capacity(messages.len());
-        let mut data: Vec<u8> = Vec::new();
-    
-        for message in messages {
-            let message_data = match message {
-                Message::StreamMessage(stream_message) => {
-                    let mut writer = Cursor::new(Vec::new());
-                    writer.write_be(&stream_message).unwrap();
-                    writer.into_inner()
-                },
-                Message::CreateSocketMessage(create_socket_message) => {
-                    let mut writer = Cursor::new(Vec::new());
-                    writer.write_be(&create_socket_message).unwrap();
-                    writer.into_inner()
-                },
-                Message::CloseSocketMessage(close_socket_message) => {
-                    let mut writer = Cursor::new(Vec::new());
-                    writer.write_be(&close_socket_message).unwrap();
-                    writer.into_inner()
-                },
-            };
-    
-            sizes.push(message_data.len() as u32);
-            data.extend(message_data);
-        }
-    
-        Self {
-            sizes,
-            data
-        }
-    }
-
-    pub fn to_bytes(&self) -> Vec<u8> {
-        // Write the data to a multiple messages buffer
-        let buffer = Vec::with_capacity(1 + self.sizes.len() * 4 + self.data.len());
-        let mut writer = Cursor::new(buffer);
-
-        writer.write_be(&self).unwrap();
-
-        writer.into_inner()
-    }
-
-    pub fn from_bytes(buffer: Vec<u8>) -> Self {
-        let mut reader = Cursor::new(buffer);
-        reader.read_be::<MultipleMessages>().unwrap()
-    }
-
-    pub fn pull_messages(&self) -> Vec<Message> {
-        let mut messages: Vec<Message> = Vec::with_capacity(self.sizes.len());
-
-        let mut i = 0;
-        let mut base_offset = 0;
-
-        //println!("Self.data: {:?} (Length {})", self.data, self.data.len());
-
-        loop {
-            let data_start = base_offset;
-            let data_end = base_offset + self.sizes[i] as usize;
-
-            //println!("Data start: {}", data_start);
-            //println!("Data end: {}", data_end);
-            //println!("Sizes: {:?}", self.sizes);
-            //println!("Current offset: {}", i);
-
-            // Get those bytes
-            let message_data = self.data[data_start..data_end].to_vec();
-
-            // Read to a message struct
-            //println!("Message data: {:?}", message_data);
-            let mut data_reader = Cursor::new(message_data);
-
-            let messagetype_reader = self.data[data_start];
-
-            let message_type = match messagetype_reader {
-                0 => MessageType::CreateSocket,
-                1 => MessageType::CloseSocket,
-                2 => MessageType::SendStreamPacket,
-                3 => MessageType::ReceiveStreamPacket,
-                _ => panic!("Invalid message type"),
-            };
-
-            let message = match message_type {
-                MessageType::CreateSocket => {
-                    Message::CreateSocketMessage(data_reader.read_be::<CreateSocketMessage>().unwrap())
-                },
-                MessageType::CloseSocket => {
-                    Message::CloseSocketMessage(data_reader.read_be::<CloseSocketMessage>().unwrap())
-                },
-                MessageType::SendStreamPacket => {
-                    Message::StreamMessage(data_reader.read_be::<StreamMessage>().unwrap())
-                },
-                MessageType::ReceiveStreamPacket => {
-                    Message::StreamMessage(data_reader.read_be::<StreamMessage>().unwrap())
-                },
-            };
-
-            // Push to messages
-            messages.push(message);
-
-            // Check if we should stop
-            if i > self.sizes.len() - 2 {
-                break;
-            }
-
-            i = i + 1;
-            base_offset = data_end;
-        }
-
-        messages
-    }
-}
-
-#[test]
-fn test_multiple_message_writing_reading() {
-    let mut messages: Vec<Message> = Vec::new();
-
-    messages.push(Message::CreateSocketMessage(CreateSocketMessage {
-        message_type: MessageType::CreateSocket,
-        socket_id: 5,
-        forwarding_address: 43,
-        forwarding_port: 80,
-    }));
-
-    messages.push(Message::StreamMessage(StreamMessage {
-        message_type: MessageType::SendStreamPacket,
-        socket_id: 5,
-        send_buffer_size: 0,
-        receive_buffer_size: 0,
-        packet_data: vec![1, 2, 3, 4, 5],
-        sequence_number: 0,
-    }));
-
-    messages.push(Message::CloseSocketMessage(CloseSocketMessage {
-        message_type: MessageType::CloseSocket,
-        socket_id: 5,
-    }));
-
-    let multiple_messages = MultipleMessages::from_messages(messages.clone());
-
-    let buffer = multiple_messages.to_bytes();
-
-    let multiple_messages = MultipleMessages::from_bytes(buffer);
-
-    let messages = multiple_messages.pull_messages();
-
-    assert_eq!(messages, messages);
-}
-
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, PartialEq, Clone, BorshSerialize, BorshDeserialize)]
 pub enum Message {
-    StreamMessage(StreamMessage),
-    CreateSocketMessage(CreateSocketMessage),
+    UpStreamMessage(UpStreamMessage),
+    DownStreamMessage(DownStreamMessage),
     CloseSocketMessage(CloseSocketMessage),
 }
 
-#[binrw::binrw]
-#[brw(repr(u8))]
-#[derive(Debug, PartialEq, Clone)]
-pub enum MessageType {
-    CreateSocket = 0,
-    CloseSocket = 1,
-    SendStreamPacket = 2,
-    ReceiveStreamPacket = 3,
-}
-
-#[binrw::binrw]
-#[derive(Debug, PartialEq, Clone)]
-#[brw(big)]
-pub struct StreamMessage {
-    pub message_type: MessageType,
-    pub socket_id: SocketID,
-    pub sequence_number: u32,
-
-    pub send_buffer_size: u32,
-    pub receive_buffer_size: u32,
-
-    #[bw(try_calc(u32::try_from(packet_data.len())))]
-    pub packet_length: u32,
-    #[br(count = packet_length)]
-    pub packet_data: Vec<u8>,
-}
-
-impl StreamMessage {
-    pub fn encoded(socket_id: SocketID, send_buffer_size: u32, receive_buffer_size: u32, packet_data: Vec<u8>) -> Vec<u8> {
-        let data = Self {
-            message_type: MessageType::SendStreamPacket,
-            socket_id,
-            send_buffer_size,
-            receive_buffer_size,
-            packet_data,
-            sequence_number: 0,
-        };
-
-        let mut writer = Cursor::new(Vec::new());
-        writer.write_be(&data).unwrap();
-        
-        return writer.into_inner();
-    }
-
-    pub fn from_bytes(bytes: Vec<u8>) -> Self {
-        let mut reader = Cursor::new(bytes);
-        return reader.read_be().unwrap();
-    }
-}
-
-#[test]
-fn test_stream_message() {
-    let mut rng = rand::thread_rng();
-    let packet_data: Vec<u8> = (0..rng.gen_range(0..100)).map(|_| rng.gen()).collect();
-
-    let data = StreamMessage {
-        message_type: MessageType::SendStreamPacket,
-        socket_id: 0,
-        send_buffer_size: 0,
-        receive_buffer_size: 0,
-        packet_data: packet_data,
-        sequence_number: 0,
-    };
-
-    let mut writer = Cursor::new(Vec::new());
-    writer.write_be(&data).unwrap();
-    let bytes = writer.into_inner();
-
-    let mut reader = Cursor::new(bytes);
-    let read_data = reader.read_be::<StreamMessage>().unwrap();
-
-    assert_eq!(data, read_data);
-    assert_eq!(data.packet_data, read_data.packet_data);
-}
-
-#[binrw::binrw]
-#[derive(Debug, PartialEq, Clone)]
-#[brw(big)]
-pub struct CreateSocketMessage {
-    pub message_type: MessageType,
-    pub socket_id: SocketID,
-    pub forwarding_address: IPV4,
-    pub forwarding_port: Port,
-}
-
-impl CreateSocketMessage {
-    pub fn encoded(socket_id: SocketID, forwarding_address: IPV4, forwarding_port: Port) -> Vec<u8> {
-        let data = Self {
-            message_type: MessageType::CreateSocket,
-            socket_id,
-            forwarding_address,
-            forwarding_port,
-        };
-
-        let mut writer = Cursor::new(Vec::new());
-        writer.write_be(&data).unwrap();
-        
-        return writer.into_inner();
-    }
-
-    pub fn from_bytes(bytes: Vec<u8>) -> Self {
-        let mut reader = Cursor::new(bytes);
-        return reader.read_be().unwrap();
-    }
-}
-
-#[test]
-fn test_create_socket_message() {
-    let data = CreateSocketMessage {
-        message_type: MessageType::CreateSocket,
-        socket_id: 0,
-        forwarding_address: 5,
-        forwarding_port: 0,
-    };
-
-    let mut writer = Cursor::new(Vec::new());
-    writer.write_be(&data).unwrap();
-    let bytes = writer.into_inner();
-
-    let mut reader = Cursor::new(bytes);
-    let read_data = reader.read_be::<CreateSocketMessage>().unwrap();
-
-    assert_eq!(data, read_data);
-    assert_eq!(data.forwarding_address, read_data.forwarding_address);
-    assert_eq!(data.forwarding_port, read_data.forwarding_port);
-    assert_eq!(data.socket_id, read_data.socket_id);
-    assert_eq!(data.message_type, read_data.message_type);
-    assert_eq!(read_data.message_type, MessageType::CreateSocket);
-}
-
-#[binrw::binrw]
-#[derive(Debug, PartialEq, Clone)]
-#[brw(big)]
+#[derive(BorshSerialize, BorshDeserialize, PartialEq, Debug, Clone)]
 pub struct CloseSocketMessage {
-    pub message_type: MessageType,
     pub socket_id: SocketID,
+    pub message_sequence_number: u32,
 }
 
 impl CloseSocketMessage {
-    pub fn encoded(socket_id: SocketID) -> Vec<u8> {
-        let data = Self {
-            message_type: MessageType::CloseSocket,
-            socket_id,
-        };
-
-        let mut writer = Cursor::new(Vec::new());
-        writer.write_be(&data).unwrap();
-        
-        return writer.into_inner();
+    pub fn encoded(&self) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+        let data = self.try_to_vec()?;
+        Ok(data)
     }
 
-    pub fn from_bytes(bytes: Vec<u8>) -> Self {
-        let mut reader = Cursor::new(bytes);
-        return reader.read_be().unwrap();
+    pub fn decode_from_bytes(data: &mut Vec<u8>) -> Result<Self, std::io::Error> { // Borsh returns std::io::Error
+        Self::try_from_slice(data)
     }
 }
 
 #[test]
 fn test_close_socket_message() {
-    let data = CloseSocketMessage {
-        message_type: MessageType::CloseSocket,
-        socket_id: 5,
+    let message = CloseSocketMessage {
+        socket_id: 0,
+        message_sequence_number: 0,
     };
 
-    let mut writer = Cursor::new(Vec::new());
-    writer.write_be(&data).unwrap();
-    let bytes = writer.into_inner();
+    let mut encoded = message.encoded().unwrap();
 
-    let mut reader = Cursor::new(bytes);
-    let read_data = reader.read_be::<CloseSocketMessage>().unwrap();
+    let decoded = CloseSocketMessage::decode_from_bytes(&mut encoded).unwrap();
 
-    assert_eq!(data, read_data);
-    assert_eq!(data.socket_id, read_data.socket_id);
-    assert_eq!(data.message_type, read_data.message_type);
-    assert_eq!(read_data.message_type, MessageType::CloseSocket);
+    assert_eq!(message, decoded);
+}
+
+#[derive(BorshSerialize, BorshDeserialize, PartialEq, Debug, Clone)]
+pub struct UpStreamMessage {
+    pub socket_id: SocketID,
+    pub message_sequence_number: u32,
+    pub dest_ip: IPV4,
+    pub dest_port: Port,
+    pub payload: Vec<u8>,
+}
+
+impl UpStreamMessage {
+    pub fn encoded(&self) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+        let data = self.try_to_vec()?;
+        Ok(data)
+    }
+
+    pub fn decode_from_bytes(data: &mut Vec<u8>) -> Result<Self, std::io::Error> { // Borsh returns std::io::Error
+        Self::try_from_slice(data)
+    }
+}
+
+#[test]
+fn test_upstream_message() {
+    let large_payload = vec![0; 100000];
+
+    let message = UpStreamMessage {
+        socket_id: 0,
+        message_sequence_number: 0,
+        dest_ip: 0,
+        dest_port: 0,
+        payload: large_payload,
+    };
+
+    let mut encoded = message.encoded().unwrap();
+
+    let decoded = UpStreamMessage::decode_from_bytes(&mut encoded).unwrap();
+
+    assert_eq!(message, decoded);
+}
+
+#[derive(BorshSerialize, BorshDeserialize, PartialEq, Debug, Clone)]
+pub struct DownStreamMessage {
+    pub socket_id: SocketID,
+    pub message_sequence_number: u32,
+    pub payload: Vec<u8>,
+}
+
+impl DownStreamMessage {
+    pub fn encoded(&self) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+        let data = self.try_to_vec()?;
+        Ok(data)
+    }
+
+    pub fn decode_from_bytes(data: &mut Vec<u8>) -> Result<Self, std::io::Error> { // Borsh returns std::io::Error
+        Self::try_from_slice(data)
+    }
+}
+
+#[test]
+fn test_downstream_message() {
+    let large_payload = vec![0; 100000];
+
+    let message = DownStreamMessage {
+        socket_id: 0,
+        message_sequence_number: 0,
+        payload: large_payload,
+    };
+
+    let mut encoded = message.encoded().unwrap();
+
+    let decoded = DownStreamMessage::decode_from_bytes(&mut encoded).unwrap();
+
+    assert_eq!(message, decoded);
+}
+
+pub fn generate_socket_id() -> SocketID {
+    LAST_SOCKET_ID.fetch_add(1, std::sync::atomic::Ordering::SeqCst) as SocketID
+}
+
+#[test]
+fn test_generate_socket_id() {
+    let socket_id = generate_socket_id();
+    assert_eq!(socket_id, 0);
+    let socket_id = generate_socket_id();
+    assert_eq!(socket_id, 1);
+}
+
+#[derive(BorshSerialize, BorshDeserialize, PartialEq, Debug, Clone)]
+pub struct MultipleMessagesUpstream {
+    pub stream_messages: Vec<UpStreamMessage>,
+    pub close_socket_messages: Vec<CloseSocketMessage>,
+}
+
+impl MultipleMessagesUpstream {
+    pub fn encoded(&self) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+        let data = self.try_to_vec()?;
+        Ok(data)
+    }
+
+    pub fn decode_from_bytes(data: &mut Vec<u8>) -> Result<Self, std::io::Error> { // Borsh returns std::io::Error
+        Self::try_from_slice(data)
+    }
+}
+
+#[test]
+fn test_multiple_messages_upstream() {
+    let large_payload = vec![0; 100000];
+
+    let mut stream_messages = Vec::new();
+    let mut close_socket_messages = Vec::new();
+
+    for i in 0..50 {
+        let message = UpStreamMessage {
+            socket_id: i,
+            message_sequence_number: i,
+            dest_ip: 0,
+            dest_port: 0,
+            payload: large_payload.clone(),
+        };
+
+        stream_messages.push(message);
+
+        let message = CloseSocketMessage {
+            socket_id: i,
+            message_sequence_number: i,
+        };
+
+        close_socket_messages.push(message);
+    }
+
+    let multiple_messages = MultipleMessagesUpstream {
+        stream_messages,
+        close_socket_messages,
+    };
+
+    let mut buffer = multiple_messages.encoded().unwrap();
+
+    let out = MultipleMessagesUpstream::decode_from_bytes(&mut buffer).unwrap();
+
+    assert_eq!(multiple_messages, out);
+}
+
+#[derive(BorshSerialize, BorshDeserialize, PartialEq, Debug, Clone)]
+pub struct MultipleMessagesDownstream {
+    pub stream_messages: Vec<DownStreamMessage>,
+}
+
+impl MultipleMessagesDownstream {
+    pub fn encoded(&self) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+        let data = self.try_to_vec()?;
+        Ok(data)
+    }
+
+    pub fn decode_from_bytes(data: &mut Vec<u8>) -> Result<Self, std::io::Error> { // Borsh returns std::io::Error
+        Self::try_from_slice(data)
+    }
+}
+
+#[test]
+fn test_multiple_messages_downstream() {
+    let large_payload = vec![0; 100000];
+
+    let mut stream_messages = Vec::new();
+
+    for i in 0..50 {
+        let message = DownStreamMessage {
+            socket_id: i,
+            message_sequence_number: i,
+            payload: large_payload.clone(),
+        };
+
+        stream_messages.push(message);
+    }
+
+    let multiple_messages = MultipleMessagesDownstream {
+        stream_messages,
+    };
+
+    let mut buffer = multiple_messages.encoded().unwrap();
+
+    let out = MultipleMessagesDownstream::decode_from_bytes(&mut buffer).unwrap();
+
+    assert_eq!(multiple_messages, out);
 }
