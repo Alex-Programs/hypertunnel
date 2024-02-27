d88231b8a26c36d4e841c81c5d8f10b8c3b12995
Alex <40502404+Alex-Programs@users.noreply.github.com>
2024-02-27 10:34:48 +0000

    Fix listener aborting upon incorrect message

--- Source/client-core/src/lib.rs
+++ Source/client-core/src/lib.rs
@@ -179,7 +179,6 @@ async fn tcp_listener(mut stream: TcpStream, upstream_passer_send: Sender<UpStre
                     println!("Failed to send rejection to client: {:?}", error);
                 }
             }
-            return;
         }
     }
 
